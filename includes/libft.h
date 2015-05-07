c509a680be091220c69650d59ac70c2dc3170469
Kantin LESCAUDRON <klescaud@e3r13p13.42.fr>
2015-05-07 18:00:43 +0200

    MAJ libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: klescaud <klescaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/26 02:37:43 by klescaud          #+#    #+#             */
-/*   Updated: 2015/02/04 22:15:29 by klescaud         ###   ########.fr       */
+/*   Updated: 2015/05/07 18:00:03 by klescaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,12 @@
 # include <string.h> /* for size_t type */
 # include <stdlib.h> /* for malloc() & free() */
 
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_isprint(int c);
-int		ft_isascii(int c);
-int 	ft_isalnum(int c);
-int		ft_isdigit(int c);
-int		ft_isalpha(int c);
 size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s1);
 void	ft_putchar(char c);
+void	ft_putchar_fd(char c, int fd);
 void	ft_putstr(const char *s);
-char	*ft_strrev(char *s);
-char	*ft_strcpy(char *dst, const char *src);
-int		ft_strcmp(const char *s1, const char *s2);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_atoi(const char *str);
+void	ft_putstr_fd(const char *s, int fd);
+void	ft_putendl(const char *s);
+void	ft_putendl_fd(const char *s, int fd);
 
 #endif
